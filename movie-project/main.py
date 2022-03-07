363c58478fdfb77888c29170b097d7e73473471c
Gloria Isedu <isedugloria@gmail.com>
2022-03-07 13:34:59 +0100

    Update main.py

--- movie-project/main.py
+++ movie-project/main.py
@@ -7,7 +7,7 @@ from wtforms.validators import DataRequired
 import requests
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = 'secret key'
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///movies-collection.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 Bootstrap(app)
